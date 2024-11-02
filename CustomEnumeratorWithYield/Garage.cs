264e8a091b11e4d34231d5aff4f7ab1e8b67373f
Abdalla Ahmed <abdallaahmed172125@gmail.com>
2024-11-03 00:52:45 +0300

    Custom Enumerator With Yield Return

---
+++ CustomEnumeratorWithYield/Garage.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+
+namespace CustomEnumeratorWithYield
+{
+	// Garage contains a set of Car objects.
+	public class Garage : IEnumerable
+	{
+		// System.Array already implements IEnumerator!
+		private Car[] carArray = new Car[4];
+		public Garage()
+		{
+			carArray[0] = new Car("FeeFee", 200);
+			carArray[1] = new Car("Clunker", 90);
+			carArray[2] = new Car("Zippy", 30);
+			carArray[3] = new Car("Fred", 30);
+		}
+
+		//public IEnumerator GetEnumerator()
+		//{
+		//	//This will not get thrown until MoveNext() is called
+		//	throw new Exception("This won't get called");
+		//	foreach (Car c in carArray)
+		//	{
+		//		yield return c;
+		//	}
+		//}
+
+		public IEnumerator GetEnumerator()
+		{
+			//This will get thrown immediately
+			// throw new Exception("This will get called");
+			return ActualImplementation();
+			//this is the local function and the actual IEnumerator implementation
+			IEnumerator ActualImplementation()
+			{
+				foreach (Car c in carArray)
+				{
+					yield return c;
+				}
+			}
+		}
+
+		public IEnumerable GetTheCars(bool returnReversed)
+		{
+			//do some error checking here
+			return ActualImplementation();
+			IEnumerable ActualImplementation()
+			{
+				// Return the items in reverse.
+				if (returnReversed)
+				{
+					for (int i = carArray.Length; i != 0; i--)
+					{
+						yield return carArray[i - 1];
+					}
+				}
+				else
+				{
+					// Return the items as placed in the array.
+					foreach (Car c in carArray)
+					{
+						yield return c;
+					}
+				}
+			}
+		}
+	}
+}
---
+++ CustomEnumeratorWithYield/Program.cs
@@ -0,0 +1,31 @@
+﻿using CustomEnumeratorWithYield;
+
+//Console.WriteLine("***** Fun with the Yield Keyword *****\n");
+//Garage carLot = new Garage();
+//try
+//{
+//	//Error at this time
+//	var carEnumerator = carLot.GetEnumerator();
+//}
+//catch (Exception e)
+//{
+//	Console.WriteLine($"Exception occurred on GetEnumerator");
+//}
+
+
+Console.WriteLine("***** Fun with the Yield Keyword *****\n");
+Garage carLot = new Garage();
+// Get items using GetEnumerator().
+foreach (Car c in carLot)
+{
+	Console.WriteLine("{0} is going {1} MPH",
+	c.PetName, c.CurrentSpeed);
+}
+Console.WriteLine();
+// Get items (in reverse!) using named iterator.
+foreach (Car c in carLot.GetTheCars(true))
+{
+	Console.WriteLine("{0} is going {1} MPH",
+	c.PetName, c.CurrentSpeed);
+}
+Console.ReadLine();
